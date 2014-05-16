28f530abf67de351889aaf8718a8612d3a202d9f
Bruno Rocha <rochacbruno@gmail.com>
2014-05-16 20:11:44 -0300

    first commit

---
+++ example.py
@@ -0,0 +1,69 @@
+#coding: utf-8
+
+from scrapy_model import BaseFetcherModel, CSSField, XPathField
+
+
+class TestFetcher(BaseFetcherModel):
+    photo_url = XPathField('//*[@id="content"]/div[1]/table/tr[2]/td/a')
+
+    nationality = CSSField(
+        '#content > div:nth-child(1) > table > tr:nth-child(4) > td > a',
+    )
+
+    links = CSSField(
+        '#content > div:nth-child(11) > ul > li > a.external::attr(href)',
+        auto_extract=True
+    )
+
+    def parse_photo_url(self, selector):
+        return "http://en.m.wikipedia.org/{}".format(
+            selector.xpath("@href").extract()[0]
+        )
+
+    def parse_nationality(self, selector):
+        return selector.css("::text").extract()[0]
+
+    def parse_name(self, selector):
+        return selector.extract()[0]
+
+    def post_parse(self):
+        # executed after all parsers
+        # you can load any data on to self._data
+        # access self._data and self._fields for current data
+        # self.selector contains original page
+        # self.fetch() returns original html
+        self._data.url = self.url
+
+
+class DummyModel(object):
+    """
+    For tests only, it can be a model in your database ORM
+    """
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    fetcher = TestFetcher(cache_fetch=True)
+    fetcher.url = "http://en.m.wikipedia.org/wiki/Guido_van_Rossum"
+
+    # Mappings can be loaded from a json file
+    # fetcher.load_mappings_from_file('path/to/file')
+    fetcher.mappings['name'] = {
+        "css": ("#section_0::text")
+    }
+
+    fetcher.parse()
+
+    print "Fetcher holds the data"
+    print fetcher._data.name
+    print fetcher._data
+
+    # How to populate an object
+    print "Populating an object"
+    dummy = DummyModel()
+
+    fetcher.populate(dummy, fields=["name", "nationality"])
+    # fields attr is optional
+    print dummy.nationality
+    pprint(dummy.__dict__)
---
+++ scrapy_model.py
@@ -0,0 +1,200 @@
+# coding: utf-8
+
+import requests
+from redis import Redis
+from scrapy.selector import Selector
+
+redis = Redis()
+
+
+class Storage(dict):
+    """
+    A dict that accepts [keys] or .attributes
+    >>> obj = Storage()
+    >>> obj["name"] = "Bruno"
+    >>> obj.company = "Catho"
+    >>> obj.name == obj["name]
+    >>> obj["company] == obj.company
+    """
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, attr, value):
+        self[attr] = value
+
+
+class BaseField(object):
+    """
+    Base for other selector fields
+    """
+
+    def __init__(self, query, auto_extract=False):
+        self.query = query
+        self.auto_extract = auto_extract
+        self._data = None
+        self._raw_data = None
+
+    @property
+    def value(self):
+        return self._data
+
+    def _parse(self, selector):
+        parsed = self.parse(selector)
+        if self.auto_extract:
+            return parsed.extract()
+        return parsed
+
+    def parse(self, selector):
+        raise NotImplementedError("Must be implemented in child class")
+
+    def get_identifier(self):
+        return getattr(self, 'identifier', "")
+
+    def __repr__(self):
+        return u"<{} - {} - {}>".format(
+            self.__class__.__name__, self.get_identifier(), self._data
+        )
+
+    def __str__(self):
+        return unicode(self._data)
+
+    def __unicode__(self):
+        return unicode(self._data)
+
+
+class GenericField(BaseField):
+    def __init__(self, identifier=None, value=None):
+        super(GenericField, self).__init__("")
+        self._data = value
+        self.identifier = identifier
+
+    def parse(self, selector):
+        return None
+
+
+class CSSField(BaseField):
+    def parse(self, selector):
+        return selector.css(self.query)
+
+
+class XPathField(BaseField):
+    def parse(self, selector):
+        return selector.xpath(self.query)
+
+
+class BaseFetcherModel(object):
+    """
+    fields example:
+            name = CSSField("div.perfil > div > div.perf.col-md-12 >"
+                            " div.col-md-10.desc > h1::text")
+    mappings example:
+        mappings = {
+            'name': {'css': 'div#test'},
+            'phone': {'xpath': '//phone'},
+            'location': '.location'  # assumes css
+        }
+
+
+    Any method named parsed_<field_name> will run after the data is collected
+    """
+
+    mappings = {}
+
+    def __init__(self, url=None, mappings=None, cache_fetch=False):
+        self.load_fields()
+        self.url = url
+        self._data = Storage()
+        self._selector = None
+        self.mappings = mappings or self.mappings.copy()
+        self.cache_fetch = cache_fetch
+
+    def load_fields(self):
+        self._fields = []
+        for name, field in self.__class__.__dict__.items():
+            if isinstance(field, BaseField):
+                field.identifier = name
+                self._fields.append(field)
+
+    def fetch(self, url=None):
+        url = self.url or url
+        cached = redis.get(url)
+        if cached and self.cache_fetch:
+            return cached
+        response = requests.get(url)
+        if self.cache_fetch:
+            redis.set(url, response.content, ex=1800)
+        return response.content
+
+    @property
+    def selector(self, refresh=False):
+        if not self._selector or refresh:
+            self._selector = Selector(text=self.fetch())
+        return self._selector
+
+    def parse(self, selector=None):
+        """
+        The entry point
+        fetcher = Fetcher(url="http://...")
+        fetcher.parse()
+        """
+
+        selector = selector or self.selector
+
+        for field in self._fields:
+            data = field._parse(selector)
+            self._data[field.identifier] = field._raw_data = data
+
+        # mappings has always the priority
+        for field_name, query in self.mappings.items():
+            if isinstance(query, dict):
+                method = query.keys()[0]
+                path = query.values()[0]
+            else:
+                method = 'css'
+                path = query
+            self._data[field_name] = getattr(selector, method)(path)
+
+        self.run_field_parsers()
+
+        for field in self._fields:
+            field._data = self._data.get(field.identifier)
+
+        self.post_parse()
+
+        self.load_generic_fields()
+
+    def load_generic_fields(self):
+        for k, v in self._data.items():
+            if not k in self._fields:
+                field = GenericField(k, v)
+                self._fields.append(field)
+                setattr(self, k, field)
+
+    def post_parse(self):
+        pass
+
+    def run_field_parsers(self):
+        self._raw_data = self._data.copy()
+        for field_name, raw_selector in self._data.items():
+            field_parser = getattr(self, 'parse_%s' % field_name, None)
+            if field_parser:
+                parsed_data = field_parser(raw_selector)
+                self._data[field_name] = parsed_data
+
+    def populate(self, obj, fields=None):
+        fields = fields or self._data.keys()
+        for field in fields:
+            setattr(obj, field, self._data.get(field))
+
+    def load_mappings_from_file(self, path_or_file):
+        """
+        Will take a JSON file object, string or path
+        and loads on to self.mappings
+         mappings = {
+            'name': {'css': 'div#test'},
+            'phone': {'xpath': '//phone'},
+            'location': '.location'  # assumes css
+        }
+        """
+        raise NotImplementedError("Its on Roadmap...")
